01611e20b827853f2e97901d1a41620a16fe493e
Elias Eriksson <elias.eriksson@elev.ga.ntig.se>
2025-05-07 12:56:20 +0200

    Initial commit

---
+++ src/MulticastChatServer.java
@@ -0,0 +1,12 @@
+
+public class MulticastChatServer {
+    public static void main(String[] args)
+        throws Exception {
+
+        //Portnummer vi använder
+        //Vi väljer 1050 eftersom en kontroll säger att den inte är upptagen  och inte reserverad för specifika tjänster.
+        int portNumber = 1050;
+        
+
+        }
+}
